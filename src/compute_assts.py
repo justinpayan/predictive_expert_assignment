237e328e6e7351169c55b39249ab6b6320d69790
Justin Payan <jpayan@umass.edu>
2024-04-24 16:29:30 -0400

    Ready to run with the updated baselines

--- src/compute_assts.py
+++ src/compute_assts.py
@@ -16,10 +16,14 @@ def main(args):
     # user_rep_scores
     data_dir = os.path.join(base_dir, "data", "%s.stackexchange.com" % topic, "npy")
     asst_scores = np.load(os.path.join(data_dir, "asst_scores.npy"))
+    asst_scores_badges = np.load(os.path.join(data_dir, "asst_scores_badges.npy"))
+    asst_scores_user_embs = np.load(os.path.join(data_dir, "asst_scores_user_embs.npy"))
+
     covs = pickle.load(open(os.path.join(data_dir, "covs.pkl"), 'rb'))
     loads = pickle.load(open(os.path.join(data_dir, "loads.pkl"), 'rb'))
     user_rep_scores = np.load(os.path.join(data_dir, "user_rep_scores.npy"))
     kp_matching_scores = np.load(os.path.join(data_dir, "kp_matching_scores.npy"))
+    sim_scores = np.load(os.path.join(data_dir, "sim_scores.npy"))
 
     # Select a subset of users and questions to work with (maybe like .5 or .6
     # fraction of each?)
@@ -37,12 +41,18 @@ def main(args):
 
         asst_scores = asst_scores[chosen_experts, :]
         asst_scores = asst_scores[:, chosen_queries]
+        asst_scores_badges = asst_scores_badges[chosen_experts, :]
+        asst_scores_badges = asst_scores_badges[:, chosen_queries]
+        asst_scores_user_embs = asst_scores_user_embs[chosen_experts, :]
+        asst_scores_user_embs = asst_scores_user_embs[:, chosen_queries]
         covs = covs[chosen_queries]
         loads = loads[chosen_experts]
         user_rep_scores = user_rep_scores[chosen_experts, :]
         user_rep_scores = user_rep_scores[:, chosen_queries]
         kp_matching_scores = kp_matching_scores[chosen_experts, :]
         kp_matching_scores = kp_matching_scores[:, chosen_queries]
+        sim_scores = sim_scores[chosen_experts, :]
+        sim_scores = sim_scores[:, chosen_queries]
 
         # Now just compute all the assignments, and save them out.
         print("Data loaded. Starting allocations", flush=True)
@@ -50,12 +60,22 @@ def main(args):
         print("Finished with pred asst, est_usw is ", est_usw, flush=True)
         np.save(os.path.join(data_dir, "alloc_%d.npy" % seed), alloc)
 
+        est_usw, alloc = solve_usw_gurobi(asst_scores_badges, covs, loads)
+        print("Finished with pred asst with badges, est_usw is ", est_usw, flush=True)
+        np.save(os.path.join(data_dir, "alloc_badges_%d.npy" % seed), alloc)
+
+        est_usw, alloc = solve_usw_gurobi(asst_scores_user_embs, covs, loads)
+        print("Finished with pred asst with user embs, est_usw is ", est_usw, flush=True)
+        np.save(os.path.join(data_dir, "alloc_user_embs_%d.npy" % seed), alloc)
+
         # for lam in np.arange(0, 1.01, .1):
         for lam in range(11):
             print("Starting on lambda=", lam, flush=True)
             lambda_val = lam*.1
             non_pred_scores = lambda_val * user_rep_scores / np.max(user_rep_scores)
-            non_pred_scores += (1 - lambda_val) * kp_matching_scores / np.max(kp_matching_scores)
+            # non_pred_scores += (1 - lambda_val) * kp_matching_scores / np.max(kp_matching_scores)
+            non_pred_scores += (1 - lambda_val) * sim_scores / np.max(sim_scores)
+
             _, alloc_non_pred = solve_usw_gurobi(non_pred_scores, covs, loads)
             np.save(os.path.join(data_dir, "alloc_non_pred_%d_%d.npy" % (lam, seed)), alloc_non_pred)
 
